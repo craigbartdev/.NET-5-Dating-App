7a279eabc9bf761f3c2004f9819ba57bc874ada9
wavydomez <craigbartjr@yahoo.com>
2020-10-11 16:53:50 -0700

    Added Angular to Walking Skeleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +52,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
