fc2647989d931d824812d45639fad2d255903469
wavydomez <craigbartjr@yahoo.com>
2020-10-14 03:08:21 -0700

    Added Backend Authentication

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
