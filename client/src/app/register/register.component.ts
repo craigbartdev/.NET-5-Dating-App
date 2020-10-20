2b7ca603a65467f76e51bba0fbceb9d0097e6c6e
wavydomez <craigbartjr@yahoo.com>
2020-10-20 16:06:03 -0700

    Login and Register for SPA

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  // @Input() usersFromHomeComponent: any
+  @Output() cancelRegister = new EventEmitter()
+
+  model: any = {}
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe(res => {
+      console.log(res);
+      this.cancel();
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
