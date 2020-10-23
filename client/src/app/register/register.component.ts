2c375bd5a38af82caceaca987450a4d1e803e72b
wavydomez <craigbartjr@yahoo.com>
2020-10-23 02:59:24 -0700

    Routing, Toastr, and Bootswatch theme

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -7,12 +8,11 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  // @Input() usersFromHomeComponent: any
   @Output() cancelRegister = new EventEmitter()
 
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -21,8 +21,9 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.model).subscribe(res => {
       console.log(res);
       this.cancel();
-    }, err => {
-      console.log(err);
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
