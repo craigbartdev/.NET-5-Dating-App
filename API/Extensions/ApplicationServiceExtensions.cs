4c65eecc994ff8c1d0858c966821fd1fc6880ba7
wavydomez <craigbartjr@yahoo.com>
2020-11-16 03:03:11 -0800

    Paginate, filter, sort, date/timeago pipes, cache

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -21,6 +21,7 @@ namespace API.Extensions
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<LogUserActivity>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
             return services;
